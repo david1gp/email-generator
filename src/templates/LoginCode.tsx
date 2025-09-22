c525e78d73c87336936c67b4af8226445590c896
David Siewert <david1gruppenplan@gmail.com>
2025-09-22 13:49:40 +0600

    refactor: replace explicitly created types with ones inferred from valibot schema

--- src/templates/LoginCode.tsx
+++ src/templates/LoginCode.tsx
@@ -1,25 +1,27 @@
-import { language, type Language } from "@/i18n/language"
+import { language } from "@/i18n/language"
+import { languageSchema } from "@/i18n/languageSchema"
 import { tt0, tt1 } from "@/i18n/tt0"
 import { CodeBlock } from "@/templates/parts/CodeBlock"
 import Footer from "@/templates/parts/Footer"
 import { LinkButton } from "@/templates/parts/LinkButton"
 import { t4emailSignIn } from "@/templates/t4emailSignIn"
 import { Body, Container, Head, Heading, Html, Preview, Section, Tailwind, Text } from "@react-email/components"
+import * as v from "valibot"
 
-export interface LoginCodeProps {
-  l?: Language
-  code?: string
-  url: string
-  // footer
-  homepageText: string
-  homepageUrl: string
-  mottoText: string
-}
+const loginCodeSchema = v.object({
+  l: v.fallback(languageSchema, language.en),
+  code: v.string(),
+  url: v.string(),
+  homepageText: v.string(),
+  homepageUrl: v.string(),
+  mottoText: v.string(),
+})
+
+export type LoginCodeProps = v.InferOutput<typeof loginCodeSchema>
 
 export function LoginCode(p: LoginCodeProps) {
-  const l = p.l ?? language.en
-  const code = p.code ?? ""
-  const previewText = tt1(l, t4emailSignIn.Sign_in_preview_x, code)
+  const l = p.l
+  const previewText = tt1(l, t4emailSignIn.Sign_in_preview_x, p.code)
 
   return (
     <Html>
@@ -34,7 +36,7 @@ export function LoginCode(p: LoginCodeProps) {
             <Text className={"text-2xl font-bold mt-0"}>{tt0(l, t4emailSignIn.Sign_in_title)}</Text>
             <Text>{tt0(l, t4emailSignIn.Sign_in_instructions)}</Text>
             <Section className={"w-full"}>
-              <CodeBlock className={"px-2"} text={code} />
+              <CodeBlock className={"px-2"} text={p.code} />
             </Section>
             <Section className={"pt-4"}>
               <LinkButton url={p.url} text={tt0(l, t4emailSignIn.Sign_in_link)} />
--- src/templates/RegisterEmail.tsx
+++ src/templates/RegisterEmail.tsx
@@ -1,26 +1,27 @@
-import { language, type Language } from "@/i18n/language"
+import { language } from "@/i18n/language"
+import { languageSchema } from "@/i18n/languageSchema"
 import { tt0, tt1 } from "@/i18n/tt0"
 import { CodeBlock } from "@/templates/parts/CodeBlock"
 import Footer from "@/templates/parts/Footer"
 import { LinkButton } from "@/templates/parts/LinkButton"
 import { t4emailSignUp } from "@/templates/t4emailSignUp"
 import { Body, Container, Head, Heading, Html, Preview, Section, Tailwind, Text } from "@react-email/components"
+import * as v from "valibot"
 
-export interface RegisterEmailProps {
-  l?: Language
-  code?: string
-  url: string
-  // footer
-  homepageText: string
-  homepageUrl: string
-  mottoText: string
-}
+const registerEmailSchema = v.object({
+  l: v.fallback(languageSchema, language.en),
+  code: v.string(),
+  url: v.string(),
+  homepageText: v.string(),
+  homepageUrl: v.string(),
+  mottoText: v.string(),
+})
+
+export type RegisterEmailProps = v.InferOutput<typeof registerEmailSchema>
 
 export function RegisterEmail(p: RegisterEmailProps) {
   const l = p.l ?? language.en
-  const code = p.code ?? ""
-  const previewText = tt1(l, t4emailSignUp.Sign_up_preview_x, code)
-  const url = p.url ?? "missing-url"
+  const previewText = tt1(l, t4emailSignUp.Sign_up_preview_x, p.code)
 
   return (
     <Html>
@@ -35,10 +36,10 @@ export function RegisterEmail(p: RegisterEmailProps) {
             <Text className={"text-2xl font-bold mt-0"}>{tt0(l, t4emailSignUp.Sign_up_title)}</Text>
             <Text>{tt0(l, t4emailSignUp.Sign_up_instructions)}</Text>
             <Section className={"w-full"}>
-              <CodeBlock className={"px-2"} text={code} />
+              <CodeBlock className={"px-2"} text={p.code} />
             </Section>
             <Section className={"pt-4"}>
-              <LinkButton url={url} text={tt0(l, t4emailSignUp.Sign_up_link)} />
+              <LinkButton url={p.url} text={tt0(l, t4emailSignUp.Sign_up_link)} />
             </Section>
           </Container>
           <Container className={"mx-auto pb-2 px-4 max-w-[600px]"}>
