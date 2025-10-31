f66d4e6e63b5972eba2fbe6bfb251172d21c98ef
David Siewert <david1gruppenplan@gmail.com>
2025-10-31 11:32:45 +0600

    fix(schema): footer: mottoText -> hompageSubtitle

--- src/server/schemas/parts/footerV1SchemaFields.ts
+++ src/server/schemas/parts/footerV1SchemaFields.ts
@@ -1,7 +1,20 @@
 import { stringSchema, stringSchema500 } from "@/server/schemas/parts/stringSchema"
+import * as v from "valibot"
+import type { FooterV1Type } from "~/index"
 
 export const footerV1SchemaFields = {
   homepageText: stringSchema,
   homepageUrl: stringSchema,
-  mottoText: stringSchema500,
+  hompageSubtitle: stringSchema500,
 } as const
+
+const footerV1Schema = v.object(footerV1SchemaFields)
+type FooterV1T = v.InferOutput<typeof footerV1Schema>
+
+function types1(a: FooterV1T): FooterV1Type {
+  return a
+}
+
+function types2(a: FooterV1Type): FooterV1T {
+  return a
+}
