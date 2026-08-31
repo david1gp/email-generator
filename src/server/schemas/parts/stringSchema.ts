7c14630ef0a4c6eb28765d72cd44e431c0a2b9a1
David Siewert <david1gruppenplan@gmail.com>
2026-08-31 19:01:32 +0200

    fix(schemas): increase text limit to 5000 characters

--- src/server/schemas/parts/stringSchema.ts
+++ src/server/schemas/parts/stringSchema.ts
@@ -1,4 +1,4 @@
 import * as a from "valibot"
 
 export const stringSchema = a.pipe(a.string(), a.trim(), a.maxLength(100))
-export const stringSchema500 = a.pipe(a.string(), a.trim(), a.maxLength(500))
+export const stringSchema500 = a.pipe(a.string(), a.trim(), a.maxLength(5000))
